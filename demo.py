14c22407f9ac845ea46b24749b078909ea208edf
Kulvin <kkulvinappa@live.com>
2021-07-05 10:40:52 +0400

    demo

---
+++ demo.py
@@ -0,0 +1,24 @@
+# import library
+import requests
+from bs4 import BeautifulSoup
+
+
+inp = input("Enter letter")
+web = requests.get(
+    'https://www.lalitmauritius.org/en/dictionary.html?letter='+inp)
+
+data = web.content
+
+soup = BeautifulSoup(data, features="html.parser")
+
+tag = soup.find_all(("div", "desc") + ("span", "main"))
+
+
+a = 1
+
+for i in tag:
+
+    print(a, ".", i.text)
+    a = a+1
+
+# print(tag)
